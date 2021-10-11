627958e776a7b2e7ac28f00bc441aed2059156c8
Martina Lee <martinalee0528@gmail.com>
2021-10-11 13:23:08 +0900

    Corona webpage api react

---
+++ react/corona api webpage1/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/Header.js'
+import Contents from './components/Contents.js'
+function App() {
+  return (
+    <div className="App">
+        <Header></Header>
+        <Contents></Contents>
+    </div>
+  );
+}
+
+export default App;
